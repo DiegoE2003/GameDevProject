8da814fcf9cd5c806bbb0edea074f6deeb293da6
DiegoE2003 <enriquezd435@gmail.com>
2024-12-04 13:55:30 -0600

    added QOL improvements, scenes

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Level1()
+    {   
+        Debug.Log("Going to lvl1");
+        SceneManager.LoadScene("Battlefield"); 
+    }
+    public void Level2(){
+        Debug.Log("Going to lvl2");
+        SceneManager.LoadScene("Battlefield2");
+    }
+    public void GoBackMenu(){
+        Debug.Log("Going back to main menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/RulesManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RulesManager : MonoBehaviour
+{
+     // Start is called before the first frame update
+    public void GoBack(){
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
